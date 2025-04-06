207cf32d97f3785c65991f1c306b45f25a9121af
Siva Rama Krishna Prasad Changala <changala.2@wright.edu>
2025-04-06 14:18:50 -0400

    used proxycurl for scraping linkedin data

---
+++ agents/linkedin_lookup_agent.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.tools import Tool
+from langchain.agents import (
+    create_react_agent,
+    AgentExecutor,
+)
+
+
+
+load_dotenv()
+
+def lookup(name:str) -> str:
+    return "https://www.linkedin.com/in/siva-rama-krishna-prasad-changala-a3115a253/"
