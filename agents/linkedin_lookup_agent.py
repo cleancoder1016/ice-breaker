ad92e8f4fd578fb352ccc49dd2b2176bc5b9f61c
Siva Rama Krishna Prasad Changala <changala.2@wright.edu>
2025-04-12 18:54:54 -0400

    Implement LinkedIn profile lookup and ice breaker functionality
    - Added linkedin_lookup function in linkedin_lookup_agent.py to fetch LinkedIn profile URLs using a language model and a custom tool.
    - Integrated linkedin_lookup in ice-breaker.py to retrieve LinkedIn data for a given name and generate a summary.
    - Created tools.py to define get_profile_url_tavily function for searching LinkedIn profiles.
    - Updated ice-breaker.py to utilize the new LinkedIn lookup and scrape functionality.
    - Removed unused code and added necessary imports for the new features.

--- agents/linkedin_lookup_agent.py
+++ agents/linkedin_lookup_agent.py
@@ -1,16 +1,51 @@
 import os
 from dotenv import load_dotenv
+from langchain.chains.summarize.refine_prompts import prompt_template
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.tools import Tool
+from langchain import hub
 from langchain.agents import (
     create_react_agent,
     AgentExecutor,
 )
-
+from tools.tools import get_profile_url_tavily
 
 
 load_dotenv()
 
-def lookup(name:str) -> str:
-    return "https://www.linkedin.com/in/siva-rama-krishna-prasad-changala-a3115a253/"
+def linkedin_lookup(name:str) -> str:
+    print(f"Entered LinkedIn Lookup with name: {name}")
+    llm = ChatOpenAI(
+        temperature=0,
+        model = "gpt-4o",
+    )
+    template = """Given the full name {name_of_person} I want you to get me a link to their LinkedIn Profile Page.
+                    Your answer should only contain a url"""
+
+    prompt_template = PromptTemplate (
+        template=template, input_variables=["name_of_person"]
+    )
+    tools_for_agent = [
+        Tool(
+            name = "Crawl google for linkedin profile page",
+            func=get_profile_url_tavily,
+            description="use when you need to get the linkedin page url",
+        )
+    ]
+
+    react_prompt = hub.pull("hwchase17/react")
+    agent = create_react_agent(llm=llm, tools=tools_for_agent, prompt=react_prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools_for_agent, verbose=True)
+
+
+    result = agent_executor.invoke(
+        input={"input": prompt_template.format_prompt(name_of_person=name)}
+    )
+
+    linkedin_profile_url = result["output"]
+    return linkedin_profile_url
+
+if __name__ == "__main__":
+    linkedin_url = linkedin_lookup(name="Siva Rama Krishna Prasad")
+    print(linkedin_url)
--- ice-breaker.py
+++ ice-breaker.py
@@ -5,15 +5,19 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
+from agents.linkedin_lookup_agent import linkedin_lookup
 from third_parties.linkendin import scrape_linkedin_profile
 
-if __name__ == "__main__":
-    """
-    load_dotenv()
-    
-    print(os.environ['COOL_API_KEY'])
-    """
-    print("Learning LangChain")
+def ice_break_with(name: str) -> str:
+
+    print("Entered Ice Breaker")
+
+    linkedin_username = linkedin_lookup(name=name)
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url=linkedin_username,
+        mock=False
+    )
+   
 
     summary_template = """
         given the linkedin information {information} about a person from I want you to create:
@@ -22,13 +26,20 @@ if __name__ == "__main__":
     """
     summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
 
-    #llm = ChatOpenAI(temperature=0, model="gpt-4o")
-    llm = ChatOllama(model='mistral')
+    llm = ChatOpenAI(temperature=0, model="gpt-4o")
+    #llm = ChatOllama(model='mistral')
     chain = summary_prompt_template | llm | StrOutputParser()
     linkedin_data = scrape_linkedin_profile(
         linkedin_profile_url="https://www.linkedin.com/in/irrelevant-for-mock-mode/",
         mock=True
     )
     res = chain.invoke(input={"information": linkedin_data})
-
     print(res)
+
+if __name__ == "__main__":
+
+    load_dotenv()
+    
+    #print(os.environ['COOL_API_KEY'])
+    
+    ice_break_with(name="Siva Rama Krishna Prasad")
